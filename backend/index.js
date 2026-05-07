a4160125762da165340e44674ebe02511d89c145
Shivani Sinha <ssaritassinha@gmail.com>
2026-05-07 20:24:19 +0530

    Deploy On vercel and Render

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-hackernews-scraper.vercel.app",
     credentials: true,
   })
 );
